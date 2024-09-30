b494cb5790e42f6677d5ac4ead4a72df422bf13f
Dilusha Lakshan <156897435+DilushaLakshan@users.noreply.github.com>
2024-09-30 14:07:19 +0530

    Completed the footer, navigation bar and made changes in css files

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
-import './FoodDisplay.css'
-import { useContext } from 'react'
-import { StoreContext } from '../../context/storeContent'
-import FoodItem from '../FoodItem/FoodItem'
+import React, { useContext } from "react";
+import "./FoodDisplay.css";
+import { StoreContext } from "../../context/storeContent";
+import FoodItem from "../FoodItem/FoodItem";
 
-const FoodDisplay = (category) => {
-
-    const {food_list} = useContext(StoreContext)
+const FoodDisplay = ({ category }) => {  // Destructure category from props
+  const { food_list } = useContext(StoreContext);
 
   return (
-    <div className='food-display' id='food-display'>
-        <h2>Top dishes near your</h2>
-        <div className="food-display-list">
-            {food_list.map((item, index)=>{
-                return <FoodItem key={index} id={item._id} name={item.name} discription={item.description} price={item.price} image={item.image}/>
-            })}
-        </div>
+    <div className="food-display" id="food-display">
+      <h2>Top dishes near you</h2> {/* Small fix in the text */}
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          {console.log(category, item.category);}
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                description={item.description}  // Fixed spelling error (discription)
+                price={item.price}
+                image={item.image}
+              />
+            );
+          }
+          return null; // Add a return statement to handle the case when category does not match
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
